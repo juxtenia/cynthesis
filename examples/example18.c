fef2f1b15887e9893a7cb835b18d2cfe71b5cd07
James <juxtenia@googlemail.com>
2017-01-19 19:44:05 +0000

    Add more examples

---
+++ examples/example18.c
@@ -0,0 +1,9 @@
+inline int foo (int i, int j, int k, int l, int m){
+    int x = (i+j) + k;
+    int y = (i+j) + l;
+    return m ? x : y;
+}
+
+int main (int argc, char *argv[]){
+    return foo(1,1,1,1,1) + foo(2,2,2,2,2);
+}
