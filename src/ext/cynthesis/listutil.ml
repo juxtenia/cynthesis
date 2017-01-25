ed6a7225a02ce135ab7df91dc93c0e20e2fdfb78
James <juxtenia@googlemail.com>
2017-01-25 18:03:24 +0000

    Add indexof utility

--- src/ext/cynthesis/listutil.ml
+++ src/ext/cynthesis/listutil.ml
@@ -1,7 +1,7 @@
-(* Takes a function f:a' -> b' option
- * and maps it to the list l:a' list
- * keeping only the Some (_) results
- * and extracting the items
+(** Takes a function f:a' -> b' option
+ *  and maps it to the list l:a' list
+ *  keeping only the Some (_) results
+ *  and extracting the items
  *)
 let mapfilter f l =
 	let rec driver acc x = match x with
@@ -11,3 +11,11 @@ let mapfilter f l =
 			| Some b -> driver (b::acc) t
 		)
 	in driver [] l
+
+(** Gives the index of item i in list l	
+ *)
+ let indexof v l = 
+ 	let rec driver i l1 = match l1 with 
+ 		| [] -> raise Not_found
+ 		| h :: t -> if h = v then i else driver (i+1) t
+ 	in driver 0 l
