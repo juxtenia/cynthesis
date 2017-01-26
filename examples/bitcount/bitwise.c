f69bb0abeb1889d1fb522671ed61e9d25a48e362
juxtenia <juxtenia@googlemail.com>
2017-01-26 22:21:46 +0000

    Optimise

--- examples/bitcount/bitwise.c
+++ examples/bitcount/bitwise.c
@@ -3,13 +3,11 @@
 int bitcount(unsigned int input);
 
 inline int bitcount (unsigned int in){
+	// Borrowed from the following, which explains why this works:
+	// http://graphics.stanford.edu/~seander/bithacks.html#CountBitsSetParallel 
 	int output = in - ((in >> 1) & 0x55555555);
-	// this is like doing (in & 0x55555555) + ((in >> 1) & 0x55555555)
-	// but takes fewer operations
 	output = ((output >> 2) & 0x33333333) + (output & 0x33333333);
-	output = ((output >> 4) + output) & 0x0F0F0F0F;
-	output = ((output >> 8) + output) & 0x00FF00FF;
-	output = ((output >> 16) + output) & 0x0000FFFF;
+	output = ((output + (output >> 4) & 0xF0F0F0F) * 0x1010101) >> 24;
 	return output;
 }
 
