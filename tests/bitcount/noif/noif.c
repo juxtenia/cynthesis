512d78b7cfb9d7b50beb831016ede945835a5c04
James Riordan <juxtenia@googlemail.com>
2017-04-04 21:00:03 +0100

    Make tester work

--- tests/bitcount/noif/noif.c
+++ tests/bitcount/noif/noif.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-int bitcount(unsigned int input);
+int bitcount(unsigned int in);
 
-inline int bitcount (unsigned int input){
+inline int bitcount (unsigned int in){
 	int output = 0;
-	for(int i = 0;i<8*sizeof(input);i++){
-		output+= ((input >> i) & 1) ;
+	for(int i = 0;i<8*sizeof(in);i++){
+		output+= ((in >> i) & 1) ;
 	}
 	return output;
 }
--- tests/bitcount/noifunroll2/noifunroll2.c
+++ tests/bitcount/noifunroll2/noifunroll2.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-int bitcount(unsigned int input);
+int bitcount(unsigned int in);
 
-inline int bitcount (unsigned int input){
+inline int bitcount (unsigned int in){
 	int output = 0;
-	for(int i = 0;i<8*sizeof(input);i+=2){
-		int tmp = (input >> i);
+	for(int i = 0;i<8*sizeof(in);i+=2){
+		int tmp = (in >> i);
 		output+= (tmp & 1) ;
 		output+= ((tmp >> 1) & 1);
 	}
